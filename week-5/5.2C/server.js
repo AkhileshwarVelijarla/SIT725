a35bc3fb4b31c77489521c33d73503abefca191b
AkhileshVelijarla <akhileshvelijarla@gmail.com>
2025-08-12 19:07:03 +1000

    changes in 5.2C

--- week-5/5.2C/server.js
+++ week-5/5.2C/server.js
@@ -2,16 +2,16 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Import route file
+
 const matchRoutes = require('./routes/matchesroutes');
 
-// Middleware for JSON (future-proof if you add POST later)
+
 app.use(express.json());
 
-// Use routes
+
 app.use('/api/matches', matchRoutes);
 
-// Root route
+
 app.get('/', (req, res) => {
   res.send('Welcome to TeamX – Match Schedules API!');
 });
